040979050645745ed1eecfd5539b2a8d5a288769
kgatjens <kgatjens@gmail.com>
2013-11-24 20:34:18 -0600

    Adding first time index.php file

---
+++ index.php
@@ -0,0 +1,43 @@
+
+
+<?php
+
+/*
+*
+*	Class Bacteria
+*	Read data from the NCBI open FTP, and save it locally. 
+*	Author: Kenneth Brenes	
+*	Organization: UCR
+*	https://github.com/kgatjens/bio_ftp.git
+*/
+
+define("SERVER_PATH","ftp://ftp.ncbi.nlm.nih.gov/genomes/Bacteria/");
+define("GENES_READS","10");
+define("FILE_NAME","NC_008598.ptt"); // Temporal
+define("FILE_EXTENSION","ptt");
+
+
+class Bacteria{
+
+	public $bacteria_name;
+
+	function __construct($bacteria_name) {
+       echo $this->bacteria_name = $bacteria_name;
+   	}
+
+   	/*
+   	* Read FTP specific file from a location
+   	* return 
+	*/
+	function readFtp(){
+
+	}
+
+
+
+}
+
+$bacteria = new Bacteria("Bacillus_thuringiensis_Al_Hakam_uid58795");
+
+
+?>
