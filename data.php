8d2a850a3a3811f554cac3867210453b971877b6
Kenneth Brenes <kenneth.brenes@possible.com>
2013-12-03 18:07:22 -0600

    Adding some files

---
+++ data.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+//species, sequences, genes
+
+
+include('main.php');
+//include('template/header.php');
+
+
+
+$bacteria->bacteriaRead($_POST['bacteria']);
+
+
+
+include('template/footer.php');
+
+?>
