a92590939a975ffcbb4d63719da09a2ece665b06
Kenneth Brenes <kenneth.brenes@possible.com>
2013-12-10 20:46:10 -0600

    big work 2

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
+
+
 <?php
-
-
 
 //species, sequences, genes
 //'Bacillus_anthracis', 'Acinetobacter_baumannii', 'Bacillus_thuringiensis'
-//
 //Typeahead
-
 include('main.php');
 include('template/header.php');
+
 include('template/content.php');
 include('template/footer.php');
 
